2f7a538af2cace829744ab3eb7f11b20f26f5fb0
S. Pratham <177943762+Dinoco711@users.noreply.github.com>
2025-02-20 23:24:42 +0530

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -1,6 +1,6 @@
 // Chatbot Widget Implementation
 class ChatbotWidget {
-    constructor(serverUrl = 'https://nexobotics-chatbot.onrender.com/chat') {
+    constructor(serverUrl = 'https://nexobotics-chatbot-2.onrender.com/chat') {
         this.serverUrl = serverUrl;
         this.initialize();
     }
